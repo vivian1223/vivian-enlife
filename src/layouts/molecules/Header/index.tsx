a4d9540dabbc04690787411191acaf9624c46a95
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-08-29 19:00:19 +0800

    feat: add header component

---
+++ src/layouts/molecules/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Header = () => {
+	return <div className={styles.header}>Hi！我是 Vivian，一個跨領域轉職的前端工程師</div>;
+};
+
+export default Header;
