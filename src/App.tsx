9bd11e03442a9b893c5204eac4c8906e6230e35d
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-08-30 19:13:00 +0800

    feat: add horizon component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './App.module.css';
 import Header from './layouts/molecules/Header';
 import Profile from './layouts/molecules/Profile';
+import Horizon from './layouts/atom/Horizon';
 
 function App() {
 	return (
 		<div className={styles.App}>
 			<Header />
 			<Profile />
+			<Horizon />
 		</div>
 	);
 }
---
+++ src/layouts/atom/Horizon/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Horizon = () => (
+	<div className={styles.horizonContainer}>
+		<div className={styles.horizon} />
+	</div>
+);
+
+export default Horizon;
