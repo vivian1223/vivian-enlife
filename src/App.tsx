4320b493b90e7cd503bfa206188fa004bf3d1e3c
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-09-01 15:56:31 +0800

    fix: optimize global css setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styles from './App.module.css';
 import Header from './layouts/molecules/Header';
 import Profile from './layouts/molecules/Profile';
+import News from './layouts/molecules/News';
+import Horizon from './layouts/atom/Horizon';
 
 function App() {
 	return (
 		<div className={styles.App}>
 			<Header />
 			<Profile />
+			<Horizon />
+			<News />
 		</div>
 	);
 }
