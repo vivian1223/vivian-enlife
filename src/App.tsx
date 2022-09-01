7590776d24a4a10b45a1c4f5fb2fbd0dbb93eca7
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-09-01 14:39:40 +0800

    fix: remove horizon from home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import styles from './App.module.css';
 import Header from './layouts/molecules/Header';
 import Profile from './layouts/molecules/Profile';
-import Horizon from './layouts/atom/Horizon';
 
 function App() {
 	return (
 		<div className={styles.App}>
 			<Header />
 			<Profile />
-			<Horizon />
 		</div>
 	);
 }
