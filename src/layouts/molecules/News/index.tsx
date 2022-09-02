8091c5901d8ab887408524bd3d43dbf1560ce87c
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-09-02 14:20:29 +0800

    fix: optimize news section

--- src/layouts/molecules/News/index.tsx
+++ src/layouts/molecules/News/index.tsx
@@ -5,9 +5,26 @@ import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import styles from './index.module.css';
 import Link from '../../atom/Link';
 
-const LinkContent = () => (
+interface LinkProperty {
+	title: string;
+}
+
+const recommendationList = [
+	{
+		id: 1,
+		source: 'https://ithelp.ithome.com.tw/users/20151147/ironman/5232',
+		title: '《鐵人賽》致 JS 開發者的 FP 新手指南',
+	},
+	{
+		id: 2,
+		source: 'https://vocus.cc/article/6268d64afd897800017a368b',
+		title: '【自學程式】那些你沒搞懂的 Flexbox 及實務應用',
+	},
+];
+
+const LinkContent: React.FC<LinkProperty> = ({ title }) => (
 	<div>
-		鐵人賽：Functional Programming 新手指南
+		{title}
 		<FontAwesomeIcon className={styles.icon} icon={faArrowUpRightFromSquare as IconProp} />
 	</div>
 );
@@ -15,10 +32,11 @@ const LinkContent = () => (
 const News = () => (
 	<div className={styles.news}>
 		<h2 className={styles.newsTitle}>最新消息</h2>
-		<Link
-			source="https://ithelp.ithome.com.tw/users/20151147/ironman/5232"
-			content={<LinkContent />}
-		/>
+		<div className={styles.list}>
+			{recommendationList.map(({ id, source, title }) => (
+				<Link key={id} source={source} content={<LinkContent title={title} />} />
+			))}
+		</div>
 	</div>
 );
 
