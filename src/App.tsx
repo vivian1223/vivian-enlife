7eaa1590561301e1173c0d3dca220af6ab255c17
Vivian Yeh <u4h93u6uhu@gmail.com>
2022-09-02 10:39:35 +0800

    feat: add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './layouts/molecules/Header';
 import Profile from './layouts/molecules/Profile';
 import News from './layouts/molecules/News';
 import Horizon from './layouts/atom/Horizon';
+import Footer from './layouts/molecules/Footer';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Profile />
 			<Horizon />
 			<News />
+			<Footer />
 		</div>
 	);
 }
